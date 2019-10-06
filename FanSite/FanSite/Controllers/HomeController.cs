18a5b5c2d3d0c97342566d0e4b935d529faf1353
Trevor R <rogerst@my.lanecc.edu>
2019-10-06 02:07:27 -0700

    beta

---
+++ FanSite/FanSite/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FanSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult History()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult Stories()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult Sources()
+        {
+            return View();
+        }
+    }
+}
